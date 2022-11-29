474bf90df41fe042b89b9e76d1af01eab7a25c83
Rianne <rianne.visser@outlook.com>
2022-11-29 07:52:18 +0100

    Solve assignment

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,13 @@
-const repeatString = function() {
 
+const repeatString = function(string, num) {
+    if (num<0){
+        return 'ERROR'
+    } else {
+    return repeatedString = string.repeat(num)
+    }
 };
+
+repeatString('hey', 3);
 
 // Do not edit below this line
 module.exports = repeatString;
