cc41858d8448eb8e8e54581ba045cc5fe1efd1e0
Rianne <rianne.visser@outlook.com>
2022-11-29 19:50:45 +0100

    Solve assignment

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,9 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, ...moreArgs) {
+    let removeFromArrSet = new Set(moreArgs);
+    let newArr= arr.filter((args) => {
+        return !removeFromArrSet.has(args);
+    })
+    return newArr
 };
 
 // Do not edit below this line
