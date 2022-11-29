375f2d552d939b017c71ff4d54cc7753afd928f4
Rianne <rianne.visser@outlook.com>
2022-11-29 20:18:22 +0100

    Solve assignment

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,11 @@
-const sumAll = function() {
-
+const sumAll = function(start, end) {
+if (start<0||end<0||typeof start != 'number' ||typeof end != 'number'){
+    return 'ERROR';
+} else if (end<start) {
+    return ((start-end)+1)*(start+end)/2;
+} else {
+return ((end-start)+1)*(end+start)/2;
+}
 };
 
 // Do not edit below this line
